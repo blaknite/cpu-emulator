01754092d0c4fcd7db2712b88b98b7cfe9e443c7
Grant Colegate <blaknite@thelanbox.com.au>
2016-06-14 17:20:57 +0930

    example to perform multiplication

--- examples/arithmetic.asm
+++ examples/arithmetic.asm
@@ -16,3 +16,18 @@ SUB     LDI   0x0
         ADD   0xff0   ; add to first value
         ST    0xfff   ; store result
         JMP   DONE    ; job done
+
+; perform multiplication
+MUL     LDI   0x0
+        STC   0x0
+        ST    0xfff
+MUL2    LD    0xfff   ; load result
+        STC   0x0
+        ADD   0xff0   ; add first value to result
+        ST    0xfff   ; store result
+        LD    0xff1   ; load second value
+        STC   0x0
+        ADDI  0xf     ; subtract 1
+        JZ    DONE    ; job done
+        ST    0xff1   ; store decremented value
+        JMP   MUL2    ; loop
