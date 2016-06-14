14f97516ba07fc8bfd31a6f6ffb17fdea5a4d2bc
Grant Colegate <blaknite@thelanbox.com.au>
2016-06-14 17:19:00 +0930

    optimise and comment logic.asm

--- examples/logic.asm
+++ examples/logic.asm
@@ -8,70 +8,58 @@ RETURN  ST    0xfff
 DONE    JMP   DONE
 
 ; perform NOT
-NOT     LDI   0x0
-        STC   0x0
-        LD    0xff0
-        NORI  0x0
+NOT     LD    0xff0
+        NORI  0x0     ; NOR with zero is same as NOT
         JMP   RETURN
 
 ; perform AND
-AND     LDI   0x0
-        STC   0x0
-        LD    0xff0
-        NORI  0x0
-        ST    0xff2
+AND     LD    0xff0
+        NORI  0x0     ; NOT of first value
+        ST    0xff0
         LD    0xff1
-        NORI  0x0
-        NOR   0xff2
+        NORI  0x0     ; NOT of second value
+        NOR   0xff0   ; NOR of results
         JMP   RETURN
 
 ; perform NAND
-NAND    LDI   0x0
-        STC   0x0
-        LD    0xff0
-        NORI  0x0
-        ST    0xff2
+NAND    LD    0xff0
+        NORI  0x0     ; NOT of first value
+        ST    0xff0
         LD    0xff1
-        NORI  0x0
-        NOR   0xff2
-        NORI  0x0
+        NORI  0x0     ; NOT of second value
+        NOR   0xff0   ; NOR of results
+        NORI  0x0     ; NOT of result
         JMP   RETURN
 
 ; perform OR
-OR      LDI   0x0
-        STC   0x0
-        LD    0xff0
-        NOR   0xff1
-        NORI  0x0
+OR      LD    0xff0
+        NOR   0xff1   ; NOR of values
+        NORI  0x0     ; NOT of result
         JMP   RETURN
 
 ; perform XOR
-XOR     LDI   0x0
-        STC   0x0
-        LD    0xff0
-        NORI  0x0
+XOR     LD    0xff0
+        NORI  0x0     ; NOT of first value
         ST    0xff2
         LD    0xff1
-        NORI  0x0
-        NOR   0xff2
+        NORI  0x0     ; NOT of second value
+        NOR   0xff2   ; NOR of results
         ST    0xff2
         LD    0xff0
-        NOR   0xff1
-        NOR   0xff2
+        NOR   0xff1   ; NOR of original values
+        NOR   0xff2   ; NOR of results
         JMP   RETURN
 
 ; perform XNOR
-XNOR    LDI   0x0
-        STC   0x0
-        LD    0xff0
-        NORI  0x0
+XNOR    LD    0xff0
+        NORI  0x0     ; NOT of first value
         ST    0xff2
         LD    0xff1
-        NORI  0x0
-        NOR   0xff2
+        NORI  0x0     ; NOT of second value
+        NOR   0xff2   ; NOR of results
         ST    0xff2
         LD    0xff0
-        NOR   0xff1
-        NOR   0xff2
-        NORI  0x0
+        NOR   0xff1   ; NOR of original values
+        NOR   0xff2   ; NOR of results
+        NORI  0x0     ; NOT of result
         JMP   RETURN
